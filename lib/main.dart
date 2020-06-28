c213732582a8356722df754fdde9275be974f660
Max Goodman <maximgoodman@gmail.com>
2020-06-28 17:59:43 -0500

    added widget state

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,103 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main(){
-  runApp(myApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget{
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
   @override
   Widget build(BuildContext context){
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFavorite(){
+    setState(() {
+      if(_isFavorited){
+        _favoriteCount--;
+        _isFavorited=false;
+      }
+      else{
+        _favoriteCount++;
+        _isFavorited=true;
+      }
+    });
+  }
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
-        children: <Widget>[
+        children: [
           Expanded(
-            child:Column(
+            /*1*/
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children:[
+              children: [
+                /*2*/
                 Container(
-    padding: const EdgeInsets.only(bottom: 8),
-    child: Text(
-    'Something called a camp',
-    style: TextStyle(
-    fontWeight: FontWeight.bold,
-    ),
-    ),
-    ) ,
-    Text(
-    'Some made up town',
-    style: TextStyle(
-    color: Colors.grey[500],
-    ),
-    ),
-    ] ,
-    ),
-    ),
-    Icon(
-    Icons.star,
-        color: Colors.red[500],
-    ),
-    Text('41'),
-    ],
-    ),
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          FavoriteWidget(), 
+        ],
+      ),
     );
+
     Color color = Theme.of(context).primaryColor;
 
-    Widget buttonSection = Containter(
+    Widget buttonSection = Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildButtonColumn(color, Icons.call, 'CALL')
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE')
-          _buildButtonColumn(color, Icons.share, 'SHARE')
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
         ],
       ),
     );
@@ -59,53 +105,57 @@ class myApp extends StatelessWidget{
     Widget textSection = Container(
       padding: const EdgeInsets.all(32),
       child: Text(
-        'TEXT DESCRIBING THE BULLSHHIT LAKE',
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
         softWrap: true,
       ),
     );
 
     return MaterialApp(
-      title: 'Demo app',
-      home: Scaffold{
+      title: 'Flutter layout demo',
+      home: Scaffold(
         appBar: AppBar(
-        title: Text('Flutter Cummies'),
-    ),
-    body:ListView(
-    children:[
-      Image.asset(
-    'null',name
-    width: 600,
-    height: 240,
-    fit: BoxFit.cover,
-    ),
-    titleSection,
-    buttonSection,
-    textSection,
-    ],
-    ),
-
+          title: Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
       ),
     );
   }
 
-  Column _buildButtonColumn(Color color, IconData icon, String label){
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, color:color),
-            Container(
-              margin:const EdgeInsets.only(top:8),
-              child: Text(
-                label,
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
-                  color: color,
-                ),
-              ),
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
             ),
-          ],
-        );
-    }
+          ),
+        ),
+      ],
+    );
+  }
 }
