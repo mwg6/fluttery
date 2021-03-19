d591620814fc1e2a1ed45bd7fe249bc3ccaec3d9
Max Goodman <maximgoodman@gmail.com>
2021-03-18 20:05:02 -0500

    'added icons and interactivity for them'

--- lib/first_flutter_app.dart
+++ lib/first_flutter_app.dart
@@ -43,6 +43,19 @@ class _RandomWordsState extends State<RandomWords> {
         pair.asPascalCase,
         style:_biggerFont,
       ),
+    trailing: Icon(
+      alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }
     );
   }
 
