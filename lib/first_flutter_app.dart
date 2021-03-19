d99c088a52a48db5633c132bcb93e3d259a17219
Max Goodman <maximgoodman@gmail.com>
2021-03-18 20:15:23 -0500

    'added favorites page'

--- lib/first_flutter_app.dart
+++ lib/first_flutter_app.dart
@@ -59,11 +59,44 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushSaved(){
+      Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            final tiles = _saved.map(
+                (WordPair pair){
+                  return ListTile(
+                    title: Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    ),
+                  );
+                },
+            );
+            final divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Names'),
+              ),
+              body: ListView(children: divided,)
+            );
+          }
+        )
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('The title'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
